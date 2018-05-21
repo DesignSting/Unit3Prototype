8ceb6d29c52c66a426e91afda85f17be5ea323c3
DesignSting <39390336+DesignSting@users.noreply.github.com>
2018-05-21 22:37:24 +1000

    21/05/18 - CameraController has be fixed and is now useable - PlayerController is in the works, jump and slide need to be written

---
+++ Unit 3 Prototype/Assets/Scripts/CameraController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject flyingBox;
+    private bool isStartGame = false;
+    private bool hasStartedGame = false;
+    float startPosition;
+    private float translation;
+
+    public int duration = 30;
+    public int timeRemaining;
+    public bool isCountingDown = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+        startPosition = flyingBox.transform.position.x;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //float translation = Time.deltaTime * 10;
+        //transform.position = transform.position + new Vector3(translation, 0, 0);
+        //transform.position = transform.position + new Vector3(0.1f, 0, 0);
+
+        if(Input.GetKeyDown(KeyCode.Space) && !hasStartedGame)
+        {
+            isStartGame = true;
+            Debug.Log("Space is Down");
+            Begin();
+            hasStartedGame = true;
+        }
+
+        if(isStartGame && !isCountingDown)
+        {
+            SideCameraControl();
+        }
+	}
+
+    void SideCameraControl()
+    {
+         translation = Time.deltaTime * 10;
+         transform.position = transform.position + new Vector3(translation, 0, 0);
+
+        if(flyingBox.transform.position.x >= 60)
+        {
+            isStartGame = false;
+        }
+    }
+
+    public void Begin()
+    {
+        if (!isCountingDown)
+        {
+            timeRemaining = duration;
+            Invoke("_tick", 1f);
+        }
+    }
+
+    private void _tick()
+    {
+        Debug.Log(timeRemaining);
+        timeRemaining--;
+        if (timeRemaining > 0)
+        {
+            Invoke("_tick", 1f);
+        }
+        else
+            isCountingDown = true;
+    }
+}
---
+++ Unit 3 Prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    float currentHeight;
+    float lastHeight;
+    bool canJump = false;
+    public GameObject playerRunner;
+
+	// Use this for initialization
+	void Start ()
+    {
+        lastHeight = playerRunner.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        currentHeight = playerRunner.transform.position.y;
+		if(currentHeight != lastHeight)
+        {
+            canJump = false;
+        }
+	}
+
+    bool CanJump()
+    {
+        
+        return true;
+    }
+}
